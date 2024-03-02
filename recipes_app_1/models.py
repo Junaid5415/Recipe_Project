8a4274ca19b93ae7e9db843a31644566207ed126
@Junaid5415 <ajunaid.edu@gmail.com>
2024-03-02 17:06:35 +0530

    Recipes Registeration done

--- recipes_app_1/models.py
+++ recipes_app_1/models.py
@@ -1,13 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 
 class Recipe(models.Model):
-    recipe_name = models.CharField(max_length=100,null=True, blank=True)
+    user = models.ForeignKey(User, on_delete = models.SET_NULL, null = True, blank = True)
+    recipe_name = models.CharField(max_length  = 100,null = True, blank = True)
     recipe_desc = models.TextField(default='')
-    recipe_image = models.ImageField(upload_to='Images', null=True, blank=True)
+    recipe_image = models.ImageField(upload_to = 'Images', null = True, blank = True)
 
 
 def __str__(self):
--- recipes_app_1/views.py
+++ recipes_app_1/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 from .models import *
+
 
 # Create your views here.
 
@@ -29,18 +32,19 @@ def index(request):
 
 
 def table(request):
-
     titles = 'Table'
     title = {'title': titles}
     queryset = Recipe.objects.all()
     return render(request, 'table.html',  {'queryset' : queryset ,**title})
 
 
+
 def delete_recipe(request, id):
     queryset = Recipe.objects.get(id=id)
     queryset.delete()
     return redirect('table')
-    
+
+
 
 def edit_recipe(request, id):
     queryset = Recipe.objects.get(id=id)
@@ -64,3 +68,47 @@ def edit_recipe(request, id):
 
     return render(request, 'update.html', {'data': queryset, **name})    
 
+
+
+def user_register(request):
+
+    if request.method == 'POST':
+        data = request.POST
+
+        first_name = data.get('First_Name') 
+        last_name = data.get('Last_Name') 
+        username = data.get('user_name')
+        email = data.get('user_email', None)
+        password = data.get('user_pass')
+
+        user = User.objects.filter(username = username)
+        if user.exists():
+            messages.info(request, 'Username Already Exist')
+            return redirect('/user_register/')
+        
+        user = User.objects.create(
+            first_name = first_name, 
+            last_name = last_name,
+            username = username,
+            email = email,
+        )
+
+        user.set_password(password)
+        user.save()
+        messages.info(request, 'Account Created Successfully! Please Login')
+        return redirect('/user_login/')
+    
+    return render(request, 'register.html')
+
+
+def user_login(request):
+    user = User.objects.all()
+    if request.method == 'POST':
+        data = request.POST
+        username = data.get('user_name')
+        email = data.get('user_email')
+        
+    return render(request, 'login.html')
+
+def user_logout(request):
+    pass 
