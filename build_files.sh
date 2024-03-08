0a1e24d5e3821a41eede2a6d1f439d008c6b6490
@Junaid5415 <ajunaid.edu@gmail.com>
2024-03-08 19:56:34 +0530

    commit

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo " BUILD START"
-python3.11.5 -m pip install -r requirement.txt
-python3.11.5 manage.py collectstatic --noinput --clear
+C:/Users/Lenovo/.virtualenvs/Recipes-gRl7-pcY/Scripts/python.exe -m pip install -r requirements.txt
+C:/Users/Lenovo/.virtualenvs/Recipes-gRl7-pcY/Scripts/python.exe manage.py collectstatic --noinput --clear
 echo " BUILD END"
